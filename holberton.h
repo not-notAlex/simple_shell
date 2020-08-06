2aa21afaa23912f92e6e945969df13a2dfa75960
not-notAlex <alex.smith1815@gmail.com>
2020-08-06 21:43:01 +0000

    starter header with includes

---
+++ holberton.h
@@ -0,0 +1,12 @@
+#ifndef HOLBERTON_H_
+#define HOLBERTON_H_
+
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <fcntl.h>
+#include <unistd.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <sys/wait.h>
+
+#endif
