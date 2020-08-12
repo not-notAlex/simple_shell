acf8b149f8d32f8909b1bf39f980939000fcf84c
not-notAlex <alex.smith1815@gmail.com>
2020-08-12 22:34:24 +0000

    Fixed all bugs and added cd function

--- strhndlrs.c
+++ strhndlrs.c
@@ -74,3 +74,15 @@ int _strcmp(char *str1, char *str2)
 		return (0);
 	return (1);
 }
+
+/**
+ * _putchar - writes the character c to stdout
+ * @c: The character to print
+ *
+ * Return: On success 1.
+ * On error, -1 is returned, and errno is set appropriately.
+ */
+int _putchar(char c)
+{
+	return (write(1, &c, 1));
+}
