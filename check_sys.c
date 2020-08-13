502c4c52d4297bbca1fe60fe53f3200c22fbb5e2
GreenCyberNinja <1824@holbertonschool.com>
2020-08-13 19:58:36 +0000

    betty compliant

--- check_sys.c
+++ check_sys.c
@@ -1,5 +1,9 @@
 #include "holberton.h"
-
+/**
+* check_sys - checks if command is a local command
+* @commands: input command
+* Return: 1 or 0
+*/
 int check_sys(char **commands)
 {
 	char *ex = "exit";
@@ -8,10 +12,10 @@ int check_sys(char **commands)
 	while (ex[i])
 	{
 		if (ex[i] != commands[0][i])
-			     return (0);
+			return (0);
 		i++;
 	}
 	if (commands[0][i] == '\0')
 		return (1);
-	return(0);
+	return (0);
 }
